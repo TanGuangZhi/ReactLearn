505a802e63a960e6b62ed10cef534c6be9d98c9d
氦客船长 <53848822+TanGuangZhi@users.noreply.github.com>
2022-10-17 23:07:59 +0800

    feat(index): new component and beautiful style

--- src/pages/YeahMyKanBan/pages/FinderSimulate/index.tsx
+++ src/pages/YeahMyKanBan/pages/FinderSimulate/index.tsx
@@ -3,21 +3,38 @@ import {
   AppstoreTwoTone,
   CheckSquareTwoTone,
   ClockCircleTwoTone,
+  CodepenOutlined,
   FireTwoTone,
   FolderTwoTone,
   HighlightTwoTone,
+  LeftOutlined,
   PlusSquareTwoTone,
+  RedditOutlined,
+  RightOutlined,
+  SearchOutlined,
+  ShareAltOutlined,
+  SmallDashOutlined,
+  TagsOutlined,
+  ToolOutlined,
+  UnorderedListOutlined,
 } from '@ant-design/icons';
 import { Space } from 'antd';
 import React from 'react';
+import _ from 'lodash';
 
 const SystemOperate = () => {
   return (
-    <main>
+    <main className="p-2 pl-4">
       <Space>
-        <span>x</span>
-        <span>-</span>
-        <span>+</span>
+        <span>
+          <div className="rounded-full bg-red-500 w-3 h-3"></div>
+        </span>
+        <span>
+          <div className="rounded-full bg-yellow-500 w-3 h-3"></div>
+        </span>
+        <span>
+          <div className="rounded-full bg-green-500 w-3 h-3"></div>
+        </span>
       </Space>
     </main>
   );
@@ -27,13 +44,16 @@ const CommonCard = ({ data }: any) => {
   if (!data) return null;
 
   return (
-    <main className="mt-4">
+    <main className="mt-4 p-4">
       <section className="text-gray-400 text-xs">{data.title}</section>
       <section>
         {data.children?.map(({ icon, title }: any) => (
-          <div className="flex items-center mt-2" key={title}>
+          <div
+            className="flex items-center p-2 hover:bg-sky-800 cursor-pointer rounded shadow"
+            key={title}
+          >
             {icon || <FolderTwoTone />}
-            <span className="ml-2">{title}</span>
+            <span className="ml-2 ">{title}</span>
           </div>
         ))}
       </section>
@@ -41,101 +61,17 @@ const CommonCard = ({ data }: any) => {
   );
 };
 
-const HeaderMenu = () => {
+const Column = ({ data }: any) => {
   return (
-    <main className="flex">
-      <section>
-        <Space>
-          <span>{'<'}</span>
-          <span>{'>'}</span>
-        </Space>
-      </section>
-      <section className="ml-2">chuangzhang</section>
-      <section className="ml-2">|||</section>
-      <section className="ml-2">***</section>
-      <section className="ml-2">
-        <Space>
-          <span>share</span>
-          <span>tag</span>
-          <span>more</span>
-        </Space>
-      </section>
-      <section className="ml-2">
-        <span>search</span>
-      </section>
-    </main>
-  );
-};
-
-const MainContent = () => {
-  const mainContentData = [
-    {
-      folder: {
-        title: '文件夹',
-        children: [
-          { icon: '', title: '公共' },
-          { icon: '', title: '图片' },
-          { icon: '', title: '文稿' },
-          { icon: '', title: '下载' },
-          { icon: '', title: '音乐' },
-          { icon: '', title: '应用程序' },
-          { icon: '', title: '影片' },
-          { icon: '', title: '桌面' },
-          { icon: '', title: 'Nutstores Files' },
-          { icon: '', title: 'Parallels' },
-          { icon: '', title: 'Zotero' },
-        ],
-      },
-      file: {
-        title: '开发者',
-        children: [
-          { icon: '', title: '.bashrc' },
-          { icon: '', title: '.logseq.zip' },
-          { icon: '', title: '.wakatime.log' },
-          { icon: '', title: '.yarnrc' },
-          { icon: '', title: '.zshrc' },
-        ],
-      },
-    },
-    {
-      folder: {
-        title: '文件夹',
-        children: [
-          { icon: '', title: '公共' },
-          { icon: '', title: '图片' },
-          { icon: '', title: '文稿' },
-          { icon: '', title: '下载' },
-          { icon: '', title: '音乐' },
-          { icon: '', title: '应用程序' },
-          { icon: '', title: '影片' },
-          { icon: '', title: '桌面' },
-          { icon: '', title: 'Nutstores Files' },
-          { icon: '', title: 'Parallels' },
-          { icon: '', title: 'Zotero' },
-        ],
-      },
-      file: {
-        title: '开发者',
-        children: [
-          { icon: '', title: '.bashrc' },
-          { icon: '', title: '.logseq.zip' },
-          { icon: '', title: '.wakatime.log' },
-          { icon: '', title: '.yarnrc' },
-          { icon: '', title: '.zshrc' },
-        ],
-      },
-    },
-  ];
-  return (
-    <main className="flex">
-      {mainContentData.map((data) => (
-        <CommonCard key={data.folder} data={data} />
+    <main className="border-r-2 max-w-xs">
+      {data.map((item: any) => (
+        <CommonCard key={item.title} data={item} />
       ))}
     </main>
   );
 };
 
-export default function index() {
+const LeftMenu = () => {
   const leftMenuData = [
     {
       title: '个人收藏',
@@ -161,22 +97,97 @@ export default function index() {
       ],
     },
   ];
+  return <Column data={leftMenuData} />;
+};
+
+const HeaderMenu = () => {
+  return (
+    <main className="flex justify-between py-2 px-4">
+      <div className="flex item-center">
+        <section>
+          <Space>
+            <LeftOutlined />
+            <RightOutlined />
+          </Space>
+        </section>
+        <section className="ml-2">
+          <span className="text-lg font-bold">chuangzhang</span>
+        </section>
+      </div>
+      <div className="flex basis-2/5 justify-between">
+        <section className="ml-2">
+          <Space>
+            <UnorderedListOutlined />
+            <CodepenOutlined />
+            <RedditOutlined />
+            <SmallDashOutlined />
+          </Space>
+        </section>
+        <section className="ml-2"></section>
+        <section className="ml-2">
+          <Space>
+            <ShareAltOutlined />
+            <TagsOutlined />
+            <ToolOutlined />
+            <SearchOutlined />
+          </Space>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+const MainContent = () => {
+  const mainContentData = [
+    {
+      title: '文件夹',
+      children: [
+        { icon: '', title: '公共' },
+        { icon: '', title: '图片' },
+        { icon: '', title: '文稿' },
+        { icon: '', title: '下载' },
+        { icon: '', title: '音乐' },
+        { icon: '', title: '应用程序' },
+        { icon: '', title: '影片' },
+        { icon: '', title: '桌面' },
+        { icon: '', title: 'Nutstores Files' },
+        { icon: '', title: 'Parallels' },
+        { icon: '', title: 'Zotero' },
+      ],
+    },
+    {
+      title: '开发者',
+      children: [
+        { icon: '', title: '.bashrc' },
+        { icon: '', title: '.logseq.zip' },
+        { icon: '', title: '.wakatime.log' },
+        { icon: '', title: '.yarnrc' },
+        { icon: '', title: '.zshrc' },
+      ],
+    },
+  ];
+  return (
+    <main>
+      <Column data={mainContentData} />
+    </main>
+  );
+};
+
+export default function index() {
   return (
     <main
-      className="border bg-black text-white rounded flex"
-      style={{ height: 800 }}
+      className="text-white rounded flex"
+      style={{ height: 800, backgroundColor: '#1e1b23' }}
     >
-      <section className="border border-blue-500 basis-1/5">
+      <section className="basis-1/5">
         <SystemOperate />
-        {leftMenuData.map((item) => (
-          <CommonCard key={item.title} data={item} />
-        ))}
+        <LeftMenu />
       </section>
-      <section className="border border-red-500 flex-auto">
-        <div className="heder border border-green-500">
+      <section className="flex-auto">
+        <div>
           <HeaderMenu />
         </div>
-        <div className="main">
+        <div>
           <MainContent />
         </div>
       </section>
