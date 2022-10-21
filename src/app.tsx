9568fde6e488d6830555927318b4f25bfe1e93e0
tanguangzhi <tanguangzhi@forensix.cn>
2022-10-21 17:23:09 +0800

    feat(layout): github

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,7 @@
 // 运行时配置
 
+import { GithubOutlined } from '@ant-design/icons'
+
 // 全局初始化数据配置，用于 Layout 用户信息和权限初始化
 // 更多信息见文档：https://next.umijs.org/docs/api/runtime-config#getinitialstate
 export async function getInitialState(): Promise<{ name: string }> {
@@ -7,25 +9,28 @@ export async function getInitialState(): Promise<{ name: string }> {
 }
 
 const settings = {
-  fixSiderbar: true,
   layout: 'mix',
 }
 export const layout = () => {
   return {
     logo: 'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
     title: 'ReactLearn',
-    header: 'f',
     menu: {
       locale: false,
     },
 
-    rightContentRender: () => <div>111</div>,
+    rightContentRender: () => (
+      <section>
+        <GithubOutlined
+          style={{ fontSize: 22 }}
+          onClick={() =>
+            window.open('https://github.com/tanguangzhi/reactlearn')
+          }
+        />
+      </section>
+    ),
     // footerRender: () => <div>222</div>,
+
     ...settings,
-    avatarProps: {
-      src: 'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
-      size: 'small',
-      title: '七妮妮',
-    },
   }
 }
