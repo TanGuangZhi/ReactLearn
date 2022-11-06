8629ee9deea08169e3de9bb107b7d01eec823b49
氦客船长 <53848822+TanGuangZhi@users.noreply.github.com>
2022-11-06 21:58:50 +0800

    feat(clock): init

--- .umirc.ts
+++ .umirc.ts
@@ -1,37 +1,37 @@
-import { defineConfig } from "@umijs/max";
+import { defineConfig } from '@umijs/max'
 
 const routes = [
-  ["01 Counter", "Counter"],
-  ["01 UserList", "UserList"],
-  ["04 Timer", "Timer"],
-  ["04 SearchUserList", "SearchUserList"],
-  ["04 ThemeSwitch", "ThemeSwitch"],
-  ["06 UseCounter", "UseCounter"],
+  ['01 Counter', 'Counter'],
+  ['01 UserList', 'UserList'],
+  ['04 Timer', 'Timer'],
+  ['04 SearchUserList', 'SearchUserList'],
+  ['04 ThemeSwitch', 'ThemeSwitch'],
+  ['06 UseCounter', 'UseCounter'],
   // ['06 UseAsync', 'UseAsync'],
-  ["06 UseScroll", "UseScroll"],
-  ["06 BlogList", "BlogList"],
-  ["07 PureRedux", "PureRedux"],
-  ["07 ReduxCounter", "ReduxCounter"],
-  ["08 FilterList", "FilterList"],
-  ["08 SearchBox", "SearchBox"],
-  ["08 PriceInput", "PriceInput"],
-  ["09 ArticleView", "ArticleView"],
-  ["10 UserList", "UserList2"],
-  ["10 CounterRenderProps", "CounterRenderProps"],
-  ["10 ListWithMore", "ListWithMore"],
-  ["11 ToggleButton", "ToggleButton"],
-  ["11 UseKeyPress", "UseKeyPress"],
-  ["13 UncontrolledForm", "UncontrolledForm"],
-  ["13 UseForm", "UseForm"],
-  ["14 NiceModalExample", "NiceModalExample"],
-  ["14 UsersLayout", "UsersLayout"],
-  ["15 MyRouter", "MyRouter"],
-  ["15 NestedRouting", "NestedRouting"],
+  ['06 UseScroll', 'UseScroll'],
+  ['06 BlogList', 'BlogList'],
+  ['07 PureRedux', 'PureRedux'],
+  ['07 ReduxCounter', 'ReduxCounter'],
+  ['08 FilterList', 'FilterList'],
+  ['08 SearchBox', 'SearchBox'],
+  ['08 PriceInput', 'PriceInput'],
+  ['09 ArticleView', 'ArticleView'],
+  ['10 UserList', 'UserList2'],
+  ['10 CounterRenderProps', 'CounterRenderProps'],
+  ['10 ListWithMore', 'ListWithMore'],
+  ['11 ToggleButton', 'ToggleButton'],
+  ['11 UseKeyPress', 'UseKeyPress'],
+  ['13 UncontrolledForm', 'UncontrolledForm'],
+  ['13 UseForm', 'UseForm'],
+  ['14 NiceModalExample', 'NiceModalExample'],
+  ['14 UsersLayout', 'UsersLayout'],
+  ['15 MyRouter', 'MyRouter'],
+  ['15 NestedRouting', 'NestedRouting'],
   // ['15 TabsPage', 'TabsPage', '/:activeTab?'],
-  ["15 RouterAuth", "RouterAuth"],
-  ["16 LazyLoad", "LazyLoad"],
-  ["19 Debounce", "Debounce"],
-];
+  ['15 RouterAuth', 'RouterAuth'],
+  ['16 LazyLoad', 'LazyLoad'],
+  ['19 Debounce', 'Debounce'],
+]
 
 export default defineConfig({
   antd: {},
@@ -40,102 +40,102 @@ export default defineConfig({
   initialState: {},
   request: {},
   layout: {
-    title: "@umijs/max",
+    title: '@umijs/max',
   },
 
   routes: [
     {
-      path: "/",
-      redirect: "/home",
+      path: '/',
+      redirect: '/home',
     },
 
     {
-      name: "首页",
-      path: "/home",
-      component: "./Home",
+      name: '首页',
+      path: '/home',
+      component: './Home',
     },
-
     {
-      name: "ReactHookDemo",
-      path: "/reactHook",
+      name: 'ReactHookDemo',
+      path: '/reactHook',
       routes: routes.map((item) => {
         return {
           name: item[0],
-          path: item[0].replace(/ /g, "/"),
-          component: "./ReactHook/" + item[0].replace(/ /g, "/"),
-        };
+          path: item[0].replace(/ /g, '/'),
+          component: './ReactHook/' + item[0].replace(/ /g, '/'),
+        }
       }),
     },
-
     {
-      name: "LayoutDemo",
-      path: "/layoutDemo",
+      name: 'LayoutDemo',
+      path: '/layoutDemo',
       routes: [
         {
-          name: "CheckCard",
-          path: "checkCard",
-          component: "./LayoutDemo/CheckCard",
+          name: 'CheckCard',
+          path: 'checkCard',
+          component: './LayoutDemo/CheckCard',
         },
       ],
     },
-
     {
-      name: "DataShow",
-      path: "/dataShow",
+      name: 'DataShow',
+      path: '/dataShow',
       routes: [
         {
-          name: "ProTable",
-          path: "proTable",
-          component: "./DataShow/ProTable",
+          name: 'ProTable',
+          path: 'proTable',
+          component: './DataShow/ProTable',
         },
       ],
     },
-
     {
-      name: "DataEntry",
-      path: "/dataEntry",
+      name: 'DataEntry',
+      path: '/dataEntry',
       routes: [
         {
-          name: "LoginForm",
-          path: "loginForm",
-          component: "./DataEntry/LoginForm",
+          name: 'LoginForm',
+          path: 'loginForm',
+          component: './DataEntry/LoginForm',
         },
       ],
     },
-
     {
-      name: "YeahMyKanBan",
-      path: "/yeahMyKanBan",
+      name: 'Clock',
+      path: '/Clock',
+      component: './Clock',
+    },
+    {
+      name: 'YeahMyKanBan',
+      path: '/yeahMyKanBan',
       routes: [
         {
-          name: "MyKanBan",
-          path: "myKanBan",
-          component: "./YeahMyKanBan/MyKanBan",
+          name: 'MyKanBan',
+          path: 'myKanBan',
+          component: './YeahMyKanBan/MyKanBan',
         },
 
         {
-          name: "KanBan",
-          path: "kanBan",
-          component: "./YeahMyKanBan/KanBan",
+          name: 'KanBan',
+          path: 'kanBan',
+          component: './YeahMyKanBan/KanBan',
         },
 
         {
-          name: "FinderSimulate",
-          path: "finderSimulate",
-          component: "./YeahMyKanBan/FinderSimulate",
+          name: 'FinderSimulate',
+          path: 'finderSimulate',
+          component: './YeahMyKanBan/FinderSimulate',
         },
       ],
     },
   ],
 
-  mfsu: { strategy: "normal" },
+  mfsu: { strategy: 'normal' },
 
-  npmClient: "pnpm",
+  npmClient: 'pnpm',
   // 让所有 pages 下的 _mock.ts 文件成为 mock 文件
   mock: {
-    include: ["src/pages/**/_mock.ts"],
+    include: ['src/pages/**/_mock.ts'],
   },
   tailwindcss: {},
-});
+})
 
-console.log("  ".replace(/ /g, "/"));
+console.log('  '.replace(/ /g, '/'))
